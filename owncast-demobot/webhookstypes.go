e6b7d38028796be44502b13667b218845112ca18
unclear Paradigm <rarepublic@protonmail.com>
2021-08-12 23:28:20 +0200

    get rid of nested TYPE field in UserNameChange event as it is not needed

--- owncast-demobot/webhookstypes.go
+++ owncast-demobot/webhookstypes.go
@@ -7,63 +7,61 @@ import (
 type EventType = string
 
 const (
-	MessageSent EventType = "CHAT"
-	UserJoined EventType = "USER_JOINED"
+	MessageSent     EventType = "CHAT"
+	UserJoined      EventType = "USER_JOINED"
 	UserNameChanged EventType = "NAME_CHANGE"
-	StreamStarted EventType = "STREAM_STARTED"
-	StreamStopped EventType = "STREAM_STOPPED"
+	StreamStarted   EventType = "STREAM_STARTED"
+	StreamStopped   EventType = "STREAM_STOPPED"
 )
 
 type User struct {
-	Id				string				`json:"id"`
-	AccessToken		string				`json:"-"`
-	DisplayName		string				`json:"displayName"`
-	DisplayColor	int					`json:"displayColor"`
-	CreatedAt		time.Time			`json:"createdAt"`
-	DisabledAt		time.Time			`json:"disabledAt,omitempty"`
-	PreviousNames	[]string			`json:"previousNames"`
-	NameChangedAt	time.Time			`json:"nameChangedAt,omitempty"`
+	Id            string    `json:"id"`
+	AccessToken   string    `json:"-"`
+	DisplayName   string    `json:"displayName"`
+	DisplayColor  int       `json:"displayColor"`
+	CreatedAt     time.Time `json:"createdAt"`
+	DisabledAt    time.Time `json:"disabledAt,omitempty"`
+	PreviousNames []string  `json:"previousNames"`
+	NameChangedAt time.Time `json:"nameChangedAt,omitempty"`
 }
 
-
 type ChatMessage struct {
-	User			User 				`json:"user,omitempty"`
-	Body			string				`json:"body,omitempty"`
-	RawBody			string				`json:"rawBody,omitempty"`
-	ID				string				`json:"id,omitempty"`
-	Visible			bool				`json:"visible"`
-	Timestamp		time.Time			`json:"timestamp,omitempty"`
+	User      User      `json:"user,omitempty"`
+	Body      string    `json:"body,omitempty"`
+	RawBody   string    `json:"rawBody,omitempty"`
+	ID        string    `json:"id,omitempty"`
+	Visible   bool      `json:"visible"`
+	Timestamp time.Time `json:"timestamp,omitempty"`
 }
 
 type StreamEvent struct {
-	Summary			string				`json:"summary"`
-	Name			string				`json:"name"`
-	StreamTitle		string				`json:"streamTitle"`
+	Summary     string `json:"summary"`
+	Name        string `json:"name"`
+	StreamTitle string `json:"streamTitle"`
 }
 
 type WebhookEvent struct {
-	Type      		EventType			`json:"type"`
-	EventData 		interface{}			`json:"eventData,omitempty"`
+	Type      EventType   `json:"type"`
+	EventData interface{} `json:"eventData,omitempty"`
 }
 
 type WebhookChatEvent struct {
-	Type      		EventType			`json:"type"`
-	EventData 		ChatMessage			`json:"eventData,omitempty"`
+	Type      EventType   `json:"type"`
+	EventData ChatMessage `json:"eventData,omitempty"`
 }
 
 type WebhookStreamStartStopEvent struct {
-	Type			EventType			`json:"type"`
-	EventData 		StreamEvent			`json:"eventData,omitempty"`
+	Type      EventType   `json:"type"`
+	EventData StreamEvent `json:"eventData,omitempty"`
 }
 
 type NameChangeEvent struct {
-	Type			string				`json:"type"`
-	Id				string				`json:"id"`
-	Timestamp		time.Time			`json:"timestamp"`
-	User			User				`json:"user"`
+	Id        string    `json:"id"`
+	Timestamp time.Time `json:"timestamp"`
+	User      User      `json:"user"`
 }
 
 type NameChangeWebhookEvent struct {
-	Type			EventType			`json:"type"`
-	EventData 		NameChangeEvent		`json:"eventData,omitempty"`
+	Type      EventType       `json:"type"`
+	EventData NameChangeEvent `json:"eventData,omitempty"`
 }
