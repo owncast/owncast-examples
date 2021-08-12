f967e8903fd1d273dc9df66c1de9004bf6c112d0
unclear Paradigm <rarepublic@protonmail.com>
2021-08-12 16:56:25 +0200

    add more messages and extract messages from httpcontroller

--- owncast-demobot/messages.go
+++ owncast-demobot/messages.go
@@ -7,11 +7,33 @@ import (
 
 func GetNamechangeMessage(newUsername string) string {
 	messages := []string{
-		"%s, I like your new name a lot",
-		"What a cool name.",
+		"%s, I like your new name a lot.",
+		"'%s'... What a cool name.",
 		"%s. This is how heroes are called",
+		"Great %s, you found the name-change function",
 	}
 	return fmt.Sprintf(messages[rand.Intn(len(messages))], newUsername)
+}
+
+func GetBotIntroductionMessage() string {
+	return "May I introduce myself? I am a friendly Owncast Bot, programmed to help you around here! I'm not very smart yet."
+}
+
+func GetNameChangeHint(username string) string {
+	return "Owncast has assigned you " + username + " as Username. If you don't like it, or want to change it, you can do so by clicking the '" + username + "' label in the upper right corner."
+}
+
+func GetFurtherResourcesMessage() string {
+	return `# Here are some links for you:
+- find us on our **[Website](https://owncast.online/)**
+- join and chat with us on **[RocketChat](https://owncast.rocket.chat)**
+- read our source code on **[Github](https://github.com/owncast)**
+- check out our **[documentation](https://owncast.online/docs/)**
+- see **[how you can get your personal Owncast up and running](https://owncast.online/quickstart/)**`
+}
+
+func GetBotHelpText() string {
+	return `Beep bop, I'm a bot. [Here](https://github.com/unclearParadigm/owncast-examples/) you can find my source code.`
 }
 
 func GetStreamStartedMessage() string {
