8622214305bbb45901e76c341097fdb4248c903e
unclear Paradigm <rarepublic@protonmail.com>
2021-08-12 16:56:09 +0200

    add SendDelay to SendSystemMessage, refactor

--- owncast-demobot/owncastapiclient.go
+++ owncast-demobot/owncastapiclient.go
@@ -1,30 +1,31 @@
 package main
 
 import (
-	"log"
 	"bytes"
-	"strings"
-	"net/http"
 	"encoding/json"
- )
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
 
- func getApiUrlFor(endpoint string) string {
-	 return strings.TrimRight(DemoBotConfiguration.OwncastAddress, "/") + endpoint
- }
- 
- func SendSystemMessage(message string) {
+func getApiUrlFor(endpoint string) string {
+	return strings.TrimRight(DemoBotConfiguration.OwncastAddress, "/") + endpoint
+}
+
+func SendSystemMessage(message string, sendDelay int) {
+	time.Sleep(time.Duration(sendDelay) * time.Second)
 	postBody, _ := json.Marshal(map[string]string{
-	   "body":  message,
+		"body": message,
 	})
 	responseBody := bytes.NewBuffer(postBody)
-	var bearer = "Bearer " + DemoBotConfiguration.AccessToken
-    req, _ := http.NewRequest("POST", getApiUrlFor("/api/integrations/chat/system"), responseBody)
-    req.Header.Add("Authorization", bearer)
+	req, _ := http.NewRequest("POST", getApiUrlFor("/api/integrations/chat/system"), responseBody)
+	req.Header.Add("Authorization", "Bearer "+DemoBotConfiguration.AccessToken)
 	req.Header.Add("ContentType", "application/json")
 
-    client := &http.Client{}
-    _, err := client.Do(req)
-    if err != nil {
-        log.Println("Error on response.\n[ERROR] -", err)
-    }
- }
+	client := &http.Client{}
+	_, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+}
