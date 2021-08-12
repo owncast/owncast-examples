12855d2199b907db57f02dcaa74b1ef5850028dc
unclear Paradigm <rarepublic@protonmail.com>
2021-08-12 16:54:20 +0200

    initialize Bot Memory on Application Startup

--- owncast-demobot/main.go
+++ owncast-demobot/main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-        "log"
-        "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
-		DemoBotConfiguration = CollectDemoBotConfiguration("config.yaml")
+	InitializeBotMemory()
+	DemoBotConfiguration = CollectDemoBotConfiguration("config.yaml")
 
-		httpMultiplexer := http.NewServeMux()
-		httpMultiplexer.HandleFunc("/auto", RequireHttpPost(RequireJsonContentType(LogRequest(AutoRoute))))
-		httpMultiplexer.HandleFunc("/stream/stop", RequireHttpPost(RequireJsonContentType(LogRequest(StreamStartStop))))
-		httpMultiplexer.HandleFunc("/stream/start", RequireHttpPost(RequireJsonContentType(LogRequest(StreamStartStop))))
-		httpMultiplexer.HandleFunc("/user/join", RequireHttpPost(RequireJsonContentType(LogRequest(UserJoin))))
-		httpMultiplexer.HandleFunc("/user/message", RequireHttpPost(RequireJsonContentType(LogRequest(UserMessage))))
+	httpMultiplexer := http.NewServeMux()
+	httpMultiplexer.HandleFunc("/auto", RequireHttpPost(RequireJsonContentType(LogRequest(AutoRoute))))
+	httpMultiplexer.HandleFunc("/stream/stop", RequireHttpPost(RequireJsonContentType(LogRequest(StreamStartStop))))
+	httpMultiplexer.HandleFunc("/stream/start", RequireHttpPost(RequireJsonContentType(LogRequest(StreamStartStop))))
+	httpMultiplexer.HandleFunc("/user/join", RequireHttpPost(RequireJsonContentType(LogRequest(UserJoin))))
+	httpMultiplexer.HandleFunc("/user/message", RequireHttpPost(RequireJsonContentType(LogRequest(UserMessage))))
 
-		log.Print("Listening on " + DemoBotConfiguration.ListenAddress)
-		err := http.ListenAndServe(DemoBotConfiguration.ListenAddress, httpMultiplexer)
-        if err != nil {
-                log.Fatal(err)
-        }
+	log.Print("Listening on " + DemoBotConfiguration.ListenAddress)
+	err := http.ListenAndServe(DemoBotConfiguration.ListenAddress, httpMultiplexer)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
