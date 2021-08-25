6a4414150e1a7c3676880e1ecad6be142cc14842
Ruffy <ruffy@rtrace.io>
2021-08-25 20:53:01 +0200

    add support for sending SystemMessages to client by ClientId to ApiClient

--- owncast-demobot/owncastapiclient.go
+++ owncast-demobot/owncastapiclient.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -20,6 +21,25 @@ func SendSystemMessage(message string, sendDelay int) {
 	})
 	responseBody := bytes.NewBuffer(postBody)
 	req, _ := http.NewRequest("POST", getApiUrlFor("/api/integrations/chat/system"), responseBody)
+	req.Header.Add("Authorization", "Bearer "+DemoBotConfiguration.AccessToken)
+	req.Header.Add("ContentType", "application/json")
+
+	client := &http.Client{}
+	_, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+}
+
+func SendSystemMessageToClient(clientId uint, message string, sendDelay int) {
+	time.Sleep(time.Duration(sendDelay) * time.Second)
+	var clientIdString string = strconv.FormatUint(uint64(clientId), 10)
+
+	postBody, _ := json.Marshal(map[string]string{
+		"body": message,
+	})
+	responseBody := bytes.NewBuffer(postBody)
+	req, _ := http.NewRequest("POST", getApiUrlFor("/api/integrations/chat/system/client/"+clientIdString), responseBody)
 	req.Header.Add("Authorization", "Bearer "+DemoBotConfiguration.AccessToken)
 	req.Header.Add("ContentType", "application/json")
 
